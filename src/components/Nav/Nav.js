bc3369735993a8d8a8d3facac1215cd210b5c9cf
JackyLin1 <jlin9702@gmail.com>
2022-05-03 17:21:18 -0700

    Update: changes to Nav-bar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -11,97 +11,92 @@ import logo from "../../images/Logo2.png";
 import "./Nav.css"
 
 function Nav () {
-  const [open, setOpen] = useState(false);
-  const anchorRef = useRef(null);
 
-  const handleToggle = () => {
-    setOpen((prevOpen) => !prevOpen)
-  }
-
-  const handleClose = (event) => {
-      if(anchorRef.current && anchorRef.current.contains(event.target)) {
+    const [open, setOpen] = React.useState(false);
+    const anchorRef = React.useRef(null);
+  
+    const handleToggle = () => {
+      setOpen((prevOpen) => !prevOpen);
+    };
+  
+    const handleClose = (event) => {
+      if (anchorRef.current && anchorRef.current.contains(event.target)) {
         return;
       }
-  }
   
-  const handleListKeyDown = (event) => {
-    if (event.key === 'Tab') {
-      event.preventDefault();
       setOpen(false);
-    } else if (event.key === 'Escape') {
-      setOpen(false);
+    };
+  
+    function handleListKeyDown(event) {
+      if (event.key === 'Tab') {
+        event.preventDefault();
+        setOpen(false);
+      } else if (event.key === 'Escape') {
+        setOpen(false);
+      }
     }
-  }
-
-  const prevOpen = React.useRef(open);
-  React.useEffect(() => {
-    if (prevOpen.current === true && open === false) {
-      anchorRef.current.focus();
-    }
-
-    prevOpen.current = open;
-  }, [open]);
-
-
-return (
-  <Stack className="nav" direction='row' spacing={2}>
-    <div className="container">
-      <img src={logo} alt="logo"></img>
-      <div className="">
-        <Button
-        className="AboutBtn"
-        >About</Button>
-
-      <div>
-        <Button
-        id="catalogueBtn"
-        aria-controls={open ? 'CatalogueList' : undefined}
-        aria-expanded={open ? 'true' : undefined}
-        aria-haspopup="true"
-        onClick={handleToggle}>Catalogue</Button>
-
-        <Popper
-          open={open}
-          anchorEl={anchorRef.current}
-          role={undefined}
-          placement="bottom-start"
-          transition
-          disablePortal
-        >
-          {({ TransitionProps, placement }) => (
-            <Grow
+  
+    // return focus to the button when we transitioned from !open -> open
+    const prevOpen = React.useRef(open);
+    React.useEffect(() => {
+      if (prevOpen.current === true && open === false) {
+        anchorRef.current.focus();
+      }
+  
+      prevOpen.current = open;
+    }, [open]);
+  
+    return (
+      <Stack direction="row" spacing={3}>
+      <div className="container">
+       <img src={logo} alt="logo"></img>
+          <Button
+            ref={anchorRef}
+            id="composition-button"
+            aria-controls={open ? 'composition-menu' : undefined}
+            aria-expanded={open ? 'true' : undefined}
+            aria-haspopup="true"
+            onClick={handleToggle}
+            variant="outlined"
+            >
+            Catalogue
+          </Button>
+          <Popper
+            open={open}
+            anchorEl={anchorRef.current}
+            role={undefined}
+            placement="bottom-start"
+            transition
+            disablePortal
+          >
+            {({ TransitionProps, placement }) => (
+              <Grow
               {...TransitionProps}
               style={{
                 transformOrigin:
-                  placement === 'bottom-start' ? 'left top' : 'left bottom',
+                placement === 'bottom-start' ? 'left top' : 'left bottom',
               }}
-            >
-    
-        <Paper>
-          <ClickAwayListener onClickAway={handleClose}>
-            <MenuList
-              autoFocusItem={open}
-              id="CatalogueList"
-              aria-labelledby="catalogueBtn"
-              onKeyDown={handleListKeyDown}
               >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-                <MenuItem onClick={handleClose}>Logout</MenuItem>
-            </MenuList>
-          </ClickAwayListener>
-        </Paper>
-      </Grow>
-    )}
-        </Popper>
-      </div>
-        
-      </div>
-    </div>
-
-      
-  </Stack>
-)
-};
+                <Paper elevation={24}>
+                  <ClickAwayListener onClickAway={handleClose}>
+                    <MenuList
+                      autoFocusItem={open}
+                      id="composition-menu"
+                      aria-labelledby="composition-button"
+                      onKeyDown={handleListKeyDown}
+                      >
+                      <MenuItem onClick={handleClose}>BedRoom</MenuItem>
+                      <MenuItem onClick={handleClose}>BathRoom</MenuItem>
+                      <MenuItem onClick={handleClose}>LivingRoom</MenuItem>
+                    </MenuList>
+                  </ClickAwayListener>
+                </Paper>
+              </Grow>
+            )}
+          </Popper>
+        </div>
+      </Stack>
+    );
+  }
 
 export default Nav;
