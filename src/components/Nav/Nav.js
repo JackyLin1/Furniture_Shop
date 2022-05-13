b19f779969a5ef207937e2a9d70fe3040eefd9bc
JackyLin1 <jlin9702@gmail.com>
2022-05-12 18:43:18 -0700

    update: nav buttons

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,19 +6,21 @@ function Nav () {
 
     return (
       <div className="container">
-        <img src={logo} alt="logo"></img>
-        <ul>
-          <li>About</li>
-          <li className="dropdown">
-            <button className="dropbtn"> Catalogue </button>
-              <div className="dropdown-content"> 
-                <a>Bedroom</a>
-                <a>Bathroom</a>
-                <a>Living Room</a>
-              </div>
-          </li>
-          <li>Cotact Us</li>
-        </ul>
+        <div className="nav">
+          <img src={logo} alt="logo"></img>
+          <ul>
+            <li>About</li>
+            <li className="dropdown">
+              <button className="dropbtn"> Catalogue </button>
+                <div className="dropdown-content"> 
+                  <a>Bedroom</a>
+                  <a>Bathroom</a>
+                  <a>Living Room</a>
+                </div>
+            </li>
+            <li>Cotact Us</li>
+          </ul>
+        </div>
       </div>
     );
   }
