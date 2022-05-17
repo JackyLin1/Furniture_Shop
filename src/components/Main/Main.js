46d78dfaebc52d1b049536f4380a947aae4f4997
JackyLin1 <jlin9702@gmail.com>
2022-05-16 22:59:06 -0700

    Update: css changes

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,11 +4,19 @@ import Factory from "../../images/Factory.jpg"
 
 
 export default function Main () {
+
+  const toggleOpaque = () => {
+    
+  }
+
   return(
-    <body>
+    <body className="mainContainer">
       <div className="HomeFactory">
         <img src={Factory} alt="factory" id="front"></img>
         <div className="slogan">Make Life Comfortable</div>
+      </div>
+      <div>
+        <h1>About Us</h1>
       </div>
     </body>
   )
