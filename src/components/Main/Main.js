76993e928e6a51629a81401963ac5e82baa7b678
JackyLin1 <jlin9702@gmail.com>
2022-05-17 20:26:54 -0700

    update: main page contents

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -17,6 +17,29 @@ export default function Main () {
       </div>
       <div>
         <h1>About Us</h1>
+        
+        <div className="factory">
+          <div>
+            state of the art machinery            
+          </div>
+          <div>
+            <img src={Factory} alt="factory" id="front"></img>
+          </div>
+          </div>
+
+        <div className="factory">
+          <div>
+            <img src={Factory} alt="factory" id="front"></img>
+          </div>
+          <div>
+            Professional Team           
+          </div>
+        </div>
+      </div>
+      <div>
+
+        <h1>Contact Us</h1>
+        
       </div>
     </body>
   )
